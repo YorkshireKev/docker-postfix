10af04faf16665c7c9a6ac22f3724836d43b122e
YorkshireKev <kev@ellis-online.co.uk>
2016-04-28 22:20:05 +0100

    Update to ubuntu:16.04 and Postfix 3.1.0

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-# chane your.host.dns to the internet DNS A record and CNAME entry that resolves
+# change your.host.dns to the internet DNS A record and CNAME entry that resolves
 # to your smtp server's docker host. e.g. mail.example.com
 docker run -d --name postfix -p 25:25 --restart always yorkshirekev/postfix your.host.dns
